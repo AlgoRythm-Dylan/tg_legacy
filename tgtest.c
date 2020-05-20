d95223206f65711a5d5bc9f9b90a95946aa0ed85
AlgoRythm-Dylan <dperunko@comcast.net>
2020-05-19 23:53:21 -0400

    Made the example pretty

--- tgtest.c
+++ tgtest.c
@@ -5,44 +5,57 @@
 
 int main() {
 
+	// Start TG and store a reference to the TG Context
 	TGContext *context = TG();
 	
-	wchar_t *str = L"Hello world";
+	// Create a wide string and make the cursor invisible
+	wchar_t *str = L"Hey Terminal! 😀😀😀😀";
 	TGSetCursorVisible(false);
 
+	// Set the console title
 	TGTitle(L"TG Test 😀");
 
-	TGColor yellowForeground = TGColorCreate(TG_GREEN, TGDefaultColor.background);
+	// Create a yellow and <default background> color pair
+	TGColor yellowForeground = TGColorCreate(TG_YELLOW, TGDefaultColor.background);
 
-	TGBuffer textBuf = TGBufCreate(15, 1);
+	// Create a buffer and render the wide string to it.
+	// This way, we can take the rendered buffer and directly output it.
+	// This should be faster in theory.
+	TGBuffer textBuf = TGBufCreate(25, 1);
 	textBuf.currentAttributes.color = yellowForeground.id;
-	TGCalculateAttrs(&textBuf.currentAttributes);
-	TGBufClear(&textBuf);
-	TGBufAddString(&textBuf, str);
+	TGCalculateAttrs(&textBuf.currentAttributes); // After applying color, compile the attrs
+	TGBufClear(&textBuf); // Buffers are not cleared when they are created
+	TGBufAddString(&textBuf, str); // Actually render the string to the buffer
 
+	// Main graphics loop
 	bool running = true;
 	while (running) {
 
+		// Clear the screen, ready for a new render!
 		TGBufClear(&context->drawBuffer);
-		TGBufBlit(&textBuf, &context->drawBuffer, (TGPoint) {5, 5});
-		//TGBufAddString(&context->drawBuffer, str);
 
+		// Main input loop
 		TGInput input;
-
 		do {
 			input = TGGetInput();
-			if (input.empty) continue;
+			if (input.empty) continue; // Because of Windows, some inputs may be empty
 			if (input.eventType == TG_EVENT_KEY) {
 				if (!input.event.keyEvent.special) {
 					if (input.event.keyEvent.key == 'q') {
-						running = false;
+						running = false; // Exit graphics loop on 'q' press
 					}
 				}
 			}
 		} while (!input.last);
 
+		// Display string from pre-rendered buffer
+		TGBufBlit(&textBuf, &context->drawBuffer, (TGPoint) {5, 5});
+
+		// Show all changes
 		TGUpdate();
 	}
+
+	// Cleanup
 	TGEnd();
 	return 0;
 }
