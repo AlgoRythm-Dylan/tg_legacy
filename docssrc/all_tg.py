8c88e4f41ae2635c81e7576e2b8948c2a21babf8
AlgoRythm-Dylan <dperunko@comcast.net>
2020-06-11 15:42:09 -0400

    Docs

--- docssrc/all_tg.py
+++ docssrc/all_tg.py
@@ -68,6 +68,46 @@ doc_item_tgbufcell.table.add(('x', 'int', 'X position of the cell to update'))
 doc_item_tgbufcell.table.add(('y', 'int', 'Y position of the cell to update'))
 doc_item_tgbufcell.table.add(('CharInfo', 'TGCharInfo', 'CharInfo to set'))
 
+doc_item_tgbufclear = DocItem("TGBufClear", "Clear a buffer and set it's virtual cursor position to (0, 0)", "Function")
+doc_item_tgbufclear.short_description = "Clear a buffer"
+doc_item_tgbufclear.table.add(('Buffer', 'TGBuffer*', 'The buffer to clear'))
+
+doc_item_tgbufcreate = DocItem("TGBufCreate", "Create a new buffer", "Function")
+doc_item_tgbufcreate.short_description = "Create a new buffer"
+doc_item_tgbufcreate.table.add(('width', 'int', 'Width of new buffer'))
+doc_item_tgbufcreate.table.add(('height', 'int', 'Height of new buffer'))
+doc_item_tgbufcreate.post_text = "**Return value**: A new `TGBuffer` struct\n"
+
+doc_item_tgbufcursormove = DocItem("TGBufCursorMove", ("Move the cursor right by some amount. Negative "
++"values acceptable to go left. Will not loop back around when it reaches the end of the buffer (or the beginning)."), "Function")
+doc_item_tgbufcursormove.short_description = "Move a buffer's virtual cursor"
+doc_item_tgbufcursormove.table.add(('Buffer', 'TGBuffer*', 'The buffer to alter'))
+doc_item_tgbufcursormove.table.add(('amount', 'int', 'How many cells to move the cursor'))
+
+doc_item_tgbufcursorposition = DocItem("TGBufCursorPosition", "Set the virtual cursor position of a buffer", "Function")
+doc_item_tgbufcursorposition.short_description = "Set a buffer's virtual cursor position"
+doc_item_tgbufcursorposition.table.add(('Buffer', 'TGBuffer*', 'The buffer to alter'))
+doc_item_tgbufcursorposition.table.add(('x', 'int', 'X dimension of position'))
+doc_item_tgbufcursorposition.table.add(('y', 'int', 'Y dimension of position'))
+
+doc_item_tgbuffer = DocItem("TGBuffer", "A buffer structure holding `TGCharInfo` types", "Struct")
+doc_item_tgbuffer.short_description = "Buffer struct"
+doc_item_tgbuffer.table.add(('size', 'COORD', 'Dimensions of the buffer'))
+doc_item_tgbuffer.table.add(('buffer', 'TGCharInfo*', 'Buffer data'))
+doc_item_tgbuffer.table.add(('length', 'int', 'Buffer size'))
+doc_item_tgbuffer.table.add(('currentAttributes', 'TGAttributes', 'Default attributes to be used'))
+doc_item_tgbuffer.table.add(('virtualCursorPosition', 'COORD', 'Where new characters will be added'))
+
+doc_item_tgbuffree = DocItem("TGBufFree", "Free resources allocated to a buffer", "Function")
+doc_item_tgbuffree.short_description = "Clean up a buffer"
+doc_item_tgbuffree.table.add(('Buffer', 'TGBuffer*', 'The buffer to free'))
+
+doc_item_tgbufsize = DocItem("TGBufSize", "Resize a buffer", "Function")
+doc_item_tgbufsize.short_description = "Resize a buffer"
+doc_item_tgbufsize.table.add(('Buffer', 'TGBuffer*', 'The buffer to resize'))
+doc_item_tgbufsize.table.add(('width', 'int', 'New width of the buffer'))
+doc_item_tgbufsize.table.add(('height', 'int', 'New height of the buffer'))
+
 category_all.add(doc_item_colors)
 category_all.add(doc_item_tgcolor)
 category_all.add(doc_item_tgcolorcreate)
@@ -79,6 +119,13 @@ category_all.add(doc_item_tgbufaddstring)
 category_all.add(doc_item_tgbufaddstringattr)
 category_all.add(doc_item_tgbufattr)
 category_all.add(doc_item_tgbufcell)
+category_all.add(doc_item_tgbufclear)
+category_all.add(doc_item_tgbufcreate)
+category_all.add(doc_item_tgbufcursormove)
+category_all.add(doc_item_tgbufcursorposition)
+category_all.add(doc_item_tgbuffer)
+category_all.add(doc_item_tgbuffree)
+category_all.add(doc_item_tgbufsize)
 
 category_colors.add(doc_item_colors)
 category_colors.add(doc_item_tgcolor)
@@ -92,6 +139,13 @@ category_buffer.add(doc_item_tgbufaddstring)
 category_buffer.add(doc_item_tgbufaddstringattr)
 category_buffer.add(doc_item_tgbufattr)
 category_buffer.add(doc_item_tgbufcell)
+category_buffer.add(doc_item_tgbufclear)
+category_buffer.add(doc_item_tgbufcreate)
+category_buffer.add(doc_item_tgbufcursormove)
+category_buffer.add(doc_item_tgbufcursorposition)
+category_buffer.add(doc_item_tgbuffer)
+category_buffer.add(doc_item_tgbuffree)
+category_buffer.add(doc_item_tgbufsize)
 
 category_all.sort()
 category_colors.sort()
